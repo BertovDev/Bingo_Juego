72a5995bd0858e42b0dcec8b455401404904f01e
TordyCapz <tordycapz@gmail.com>
2020-08-01 21:11:48 -0300

    Diseño
    Se mejoro el diseño de el codigo

--- BingoTP/main.c
+++ BingoTP/main.c
@@ -6,29 +6,29 @@
 #include "procedimientos.h"
 
 int main() {
-  srand(time(NULL));
+    srand(time(NULL));
 
-  datosJugador();
-  int numeroDeCartones = cuantosCartones();
-  int matrizJugador[numeroDeCartones][3][5];  // MATRIZ DEL JUGADOR
-  int matrizPc[numeroDeCartones][3][5];       // Matriz de la PC
+    datosJugador();
+    int numeroDeCartones = cuantosCartones();
+    int matrizJugador[numeroDeCartones][3][5];  // MATRIZ DEL JUGADOR
+    int matrizPc[numeroDeCartones][3][5];       // Matriz de la PC
 
-  /*Carga de matriz, tanto del jugador como de la PC*/
+    /*Carga de matriz, tanto del jugador como de la PC*/
 
-  /*cargarMatrizDeCeros(matrizJugador, numeroDeCartones);
+    /*cargarMatrizDeCeros(matrizJugador, numeroDeCartones);
   cargarMatrizDeCeros(matrizPc, numeroDeCartones);*/
 
-  cargarMatrizAleatoriaSinRepetir(matrizPc, numeroDeCartones);
+    cargarMatrizAleatoriaSinRepetir(matrizPc, numeroDeCartones);
 
-  /*Se elige el tipo de carton*/
-  tipoDeCarton(matrizJugador, numeroDeCartones);
+    /*Se elige el tipo de carton*/
+    tipoDeCarton(matrizJugador, numeroDeCartones);
 
-  /*Se muetra la matriz del jugador y de la pc ---------> El de la pc se muestra
+    /*Se muetra la matriz del jugador y de la pc ---------> El de la pc se muestra
    * solo para testear que esta bien*/
-  limpiarPantalla();
-  mostrarMatriz(matrizJugador, numeroDeCartones);
-  printf("-------------------Cartones PC:-----------------------\n");
-  mostrarMatriz(matrizPc, numeroDeCartones);
+    limpiarPantalla();
+    mostrarMatriz(matrizJugador, numeroDeCartones);
+    printf("-------------------Cartones PC:-----------------------\n");
+    mostrarMatriz(matrizPc, numeroDeCartones);
 
-  return 0;
+    return 0;
 }
--- BingoTP/procedimientos.c
+++ BingoTP/procedimientos.c
@@ -1,181 +1,175 @@
+#include "procedimientos.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "procedimientos.h"
 #define RENGLON 3
 #define COLUMNA 5
 
-
-void limpiarPantalla(){
-    system("cls");          //Limpia la consola
+void limpiarPantalla() {
+    system("cls");  //Limpia la consola
 }
 
 void datosJugador() {
-
     char nombre[20];
     char apellido[20];
-    long int documento = 0; //Tipo de dato entero mas largo para almacenar el documento
+    long int documento = 0;  //Tipo de dato entero mas largo para almacenar el documento
     int cantCartones = 0;
     long int documentoError;
-    
+
     limpiarPantalla();
 
     printf("------Bienvenido al Bingo 3.0------\n");
     printf("\nIngrese sus datos para poder jugar !\n");
     //Datos del jugador
     printf("Ingrese su nombre: ");
-    scanf("%s",nombre);
+    scanf("%s", nombre);
     printf("Ingrese su apellido: ");
-    scanf("%s",apellido);
+    scanf("%s", apellido);
 
-        /*Se pide el numero de documento, y se corrobora que solo se ingrese numeros.*/
-    do
-    {
+    /*Se pide el numero de documento, y se corrobora que solo se ingrese numeros.*/
+    do {
         printf("Ingrese su documento: ");
-        while (getchar() != '\n');
-        
-    } while (scanf("%d", &documento) !=1);
+        while (getchar() != '\n')
+            ;
 
-    
+    } while (scanf("%d", &documento) != 1);
+
     //Fin datos del jugador
     //Fin cant cartones
 }
 
 //inicio codigo sergio
-int cuantosCartones(){
-    int opci=0;
-    int numeroDeCartones=0;
-    
+int cuantosCartones() {
+    int opci = 0;
+    int numeroDeCartones = 0;
+
     limpiarPantalla();
 
     printf(" \n\n Con cuantos cartones te gustaria jugar?\n Solo podes elegir 1, 2 o 3...");
-    scanf("%d",&opci);
-    
-    while((opci>3)||(opci<1)){
-            printf("\n!!!!!  ERROR NUMERO NO VALIDO ELIJA OTRO... \n");
-            printf(" .....CON CUANTOS CARTONES QUIERES JUGAR? CON 1 CON 2 O 3 \n");
-            scanf("%d",&opci);
+    scanf("%d", &opci);
+
+    while ((opci > 3) || (opci < 1)) {
+        printf("\n!!!!!  ERROR NUMERO NO VALIDO ELIJA OTRO... \n");
+        printf(" .....CON CUANTOS CARTONES QUIERES JUGAR? CON 1 CON 2 O 3 \n");
+        scanf("%d", &opci);
     }
     return opci;
 }
 
-void tipoDeCarton(int m[][RENGLON][COLUMNA],int opci){
+void tipoDeCarton(int m[][RENGLON][COLUMNA], int opci) {
     int opci2;
-   printf("\n\n!!!!!QUE TIPO DE CARTONES QUIERES?!!!!!!\n");
-   printf(" 1 : COMPRAR UNOS CARTONES ALEATORIOS\n");
-   printf(" 2 : COMPRAR UNOS CARTONES PERSONALIZADOS\n");
-   scanf("%d",&opci2);
+    printf("\n\n!!!!!QUE TIPO DE CARTONES QUIERES?!!!!!!\n");
+    printf(" 1 : COMPRAR UNOS CARTONES ALEATORIOS\n");
+    printf(" 2 : COMPRAR UNOS CARTONES PERSONALIZADOS\n");
+    scanf("%d", &opci2);
 
-    while ((opci2<1)||(opci2>2)) {
-            printf("!!!!!!RESPUETA NO VALIDA INGRESE UN NUMERO DE LAS OPCIONES");
-            printf("!!!!!QUE TIPO DE CARTONES QUIERES?!!!!!!\n");
-            printf(" 1 : COMPRAR UNOS CARTONES ALEATORIOS\n");
-            printf(" 2 : COMPRAR UNOS CARTONES PERSONALIZADOS\n");
-            scanf("%d",&opci2);
+    while ((opci2 < 1) || (opci2 > 2)) {
+        printf("!!!!!!RESPUETA NO VALIDA INGRESE UN NUMERO DE LAS OPCIONES");
+        printf("!!!!!QUE TIPO DE CARTONES QUIERES?!!!!!!\n");
+        printf(" 1 : COMPRAR UNOS CARTONES ALEATORIOS\n");
+        printf(" 2 : COMPRAR UNOS CARTONES PERSONALIZADOS\n");
+        scanf("%d", &opci2);
     }
 
-    if (opci2==1) {
-          cargarMatrizAleatoriaSinRepetir(m, opci);
-    }else{
-        cargarManualmenteMatriz( m ,opci);
+    if (opci2 == 1) {
+        cargarMatrizDeCeros(m, opci);
+
+    } else {
+        cargarMatrizDeCeros(m, opci);
+        cargarManualmenteMatriz(m, opci);
     }
-
 }
 
-void cargarMatrizDeCeros(int m [][RENGLON][COLUMNA],int opcion){
-    int i,r,c;
-    for (i=0;i<opcion;i++){
-            for (r=0;r<3;r++) {
-                    for (c=0;c<5;c++){
-                        m [i][r][c] = 0;
-                        }
-                }
-        }
-}
-
-int buscarNumeroRepe(int m [][RENGLON][COLUMNA],int opcion,int busco){
-    int posi = -1;
-    int i,r,c;
-    for (i=0;i<opcion;i++){
-            for (r=0;r<3;r++){
-                    for (c=0;c<5;c++){
-
-                        if (m[i][r][c] == busco){
-                            posi=c;
-                            }
-                        }
-                }
-        }
-        return posi;
-
-}
-
-void cargarMatrizAleatoriaSinRepetir(int m[][RENGLON][COLUMNA],int opcion){
-    int i,r,c;
-    int almacen=0;
-    int posicion= -1;
-     for (i=0;i<opcion;i++){
-            for (r=0;r<RENGLON;r++) {
-                    for (c=0;c<COLUMNA;c++){
-                        almacen= 1 + rand()%90;
-                        posicion=buscarNumeroRepe(m,opcion,almacen);
-                        while (posicion!= -1){
-                                almacen= 1 + rand()%90;
-                                posicion=buscarNumeroRepe(m,opcion,almacen);
-
-                        }
-                        m [i][r][c]=almacen;
-                    }
+void cargarMatrizDeCeros(int m[][RENGLON][COLUMNA], int opcion) {
+    int i, r, c;
+    for (i = 0; i < opcion; i++) {
+        for (r = 0; r < 3; r++) {
+            for (c = 0; c < 5; c++) {
+                m[i][r][c] = 0;
             }
+        }
     }
-
 }
 
-void mostrarMatriz(int m[][RENGLON][COLUMNA],int opcion){
-    int i,r,c;
+int buscarNumeroRepe(int m[][RENGLON][COLUMNA], int opcion, int busco) {
+    int posi = -1;
+    int i, r, c;
+    for (i = 0; i < opcion; i++) {
+        for (r = 0; r < 3; r++) {
+            for (c = 0; c < 5; c++) {
+                if (m[i][r][c] == busco) {
+                    posi = c;
+                }
+            }
+        }
+    }
+    return posi;
+}
+
+void cargarMatrizAleatoriaSinRepetir(int m[][RENGLON][COLUMNA], int opcion) {
+    int i, r, c;
+    int almacen = 0;
+    int posicion = -1;
+    for (i = 0; i < opcion; i++) {
+        for (r = 0; r < RENGLON; r++) {
+            for (c = 0; c < COLUMNA; c++) {
+                almacen = 1 + rand() % 90;
+                posicion = buscarNumeroRepe(m, opcion, almacen);
+                while (posicion != -1) {
+                    almacen = 1 + rand() % 90;
+                    posicion = buscarNumeroRepe(m, opcion, almacen);
+                }
+                m[i][r][c] = almacen;
+            }
+        }
+    }
+}
+
+void mostrarMatriz(int m[][RENGLON][COLUMNA], int opcion) {
+    int i, r, c;
     /*Agregar limpiar pantalla una vez terminado el muestreo de la matriz de la pc*/
 
     printf("Tus cartones son: \n\n");
 
-    for (i=0;i<opcion;i++){
-            for (r=0;r<3;r++) {
-                    for (c=0;c<5;c++){
-                            printf(" %d ", m[i][r][c]);
-                            if (m[i][r][c]<10){
-                                printf(" ");
-                            }
-                        }
-                        printf("\n");
+    for (i = 0; i < opcion; i++) {
+        for (r = 0; r < 3; r++) {
+            for (c = 0; c < 5; c++) {
+                printf(" %d ", m[i][r][c]);
+                if (m[i][r][c] < 10) {
+                    printf(" ");
                 }
-                printf("\n\n");
+            }
+            printf("\n");
         }
+        printf("\n\n");
+    }
 }
 
-void cargarManualmenteMatriz(int m [][RENGLON][COLUMNA],int opcion){
-    int i,r,c;
-    int almacen=0;
-    int posicion= -1;
-     for (i=0;i<opcion;i++){
-            for (r=0;r<RENGLON;r++) {
-                    for (c=0;c<COLUMNA;c++){
-                       printf("INGRESE EL NUMERO PARA EL CARTON %d en el renglon %d columna %d \n",i,r,c);
-                       scanf("%d",&almacen);
-                       while ((almacen>90) ||(almacen<1)){
-                            printf("!!!!!!!NUMERO NO VALIDO ENGRESE UN NUMERO ENTRE 1 Y 90 \n");
-                            printf("INGRESE EL NUMERO PARA EL CARTON %d en el renglon %d columna %d \n",i,r,c);
-                            scanf("%d",&almacen);
-                       }
-                        posicion=buscarNumeroRepe(m,opcion,almacen);
-                        while (posicion!= -1 ){
-                                printf("!!!!!EL NUMERO %d YA EXISTE INGRESE UNO NUEVO \n",almacen);
-                                printf("INGRESE EL NUMERO PARA EL CARTON %d en el renglon %d columna %d \n",i,r,c);
-                                scanf("%d",&almacen);
-                                posicion=buscarNumeroRepe(m,opcion,almacen);
-
-                        }
-                        m [i][r][c]=almacen;
-                    }
+void cargarManualmenteMatriz(int m[][RENGLON][COLUMNA], int opcion) {
+    int i, r, c;
+    int almacen = 0;
+    int posicion = -1;
+    for (i = 0; i < opcion; i++) {
+        for (r = 0; r < RENGLON; r++) {
+            for (c = 0; c < COLUMNA; c++) {
+                printf("INGRESE EL NUMERO PARA EL CARTON %d en el renglon %d columna %d \n", i, r, c);
+                scanf("%d", &almacen);
+                while ((almacen > 90) || (almacen < 1)) {
+                    printf("!!!!!!!NUMERO NO VALIDO ENGRESE UN NUMERO ENTRE 1 Y 90 \n");
+                    printf("INGRESE EL NUMERO PARA EL CARTON %d en el renglon %d columna %d \n", i, r, c);
+                    scanf("%d", &almacen);
+                }
+                posicion = buscarNumeroRepe(m, opcion, almacen);
+                while (posicion != -1) {
+                    printf("!!!!!EL NUMERO %d YA EXISTE INGRESE UNO NUEVO \n", almacen);
+                    printf("INGRESE EL NUMERO PARA EL CARTON %d en el renglon %d columna %d \n", i, r, c);
+                    scanf("%d", &almacen);
+                    posicion = buscarNumeroRepe(m, opcion, almacen);
+                }
+                m[i][r][c] = almacen;
             }
+        }
     }
 }
 
