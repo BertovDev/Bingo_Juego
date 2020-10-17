70ca8604b6221f41448a79ae75921fa3fe8ef952
BertovDev <bertobautista18@gmail.com>
2020-10-17 14:41:39 -0300

    Se elimino el TDA  ,se arreglo cantarColumna , se agrego cantarLinea , ahora ambas suman puntos al jugador

--- bingoFinalV1/main.c
+++ bingoFinalV1/main.c
@@ -6,43 +6,75 @@
 #define RENGLON 3
 #define COLUMNA 5
 #define NUMCARTONES 3
-
-struct Cartones{
+struct Cartones
+{
     int carton[RENGLON][COLUMNA];
 };
-
-struct Jugador{
+struct Jugador
+{
     char nombre[20];
     char apellido[20];
     char dni[9];
     int numDeCartones;
     struct Cartones vCarton[NUMCARTONES];
-    int puntos;
+    float puntos;
     int bolillas[90];
     int columna1[5];
     int fila[3];
 };
-
-struct JugadorPc{
+struct JugadorPc
+{
     int numDeCartones;
     struct Cartones cartonPc[NUMCARTONES];
 };
- //MUESTRA UN MENU PARA SABER CON CUANTOS CARTONES QUERES JUGAR Y DEVUELVE EL NUMERO ELEJIDO
+//PRE: NO TIENE.
+//POSTCONDICONES: MUESTRA UN MENU PARA SABER CON CUANTOS CARTONES QUERES JUGAR Y DEVUELVE EL NUMERO ELEJIDO
 int cuantosCartones();
+//PRE: NO TIENE.
+//POSTCONDICONES:
 struct Jugador cargarJugador();
+//PRE: NO TIENE.
+//POSTCONDICONES:
 void mostrarJugadores(struct Jugador p, struct JugadorPc maquina);
+//PRE: NO TIENE.
+//POSTCONDICONES:
 int buscarNumeroRepe(int m[RENGLON][COLUMNA],int busco);
+//PRE: NO TIENE.
+//POSTCONDICONES:
 struct Cartones cargarCarton(int tipo);
+//PRE: NO TIENE.
+//POSTCONDICONES:
 void mostrarCarton(struct Cartones e);
+//PRE: NO TIENE.
+//POSTCONDICONES:
 void cargarCatones(struct Cartones c[],int opcion);
+//PRE: NO TIENE.
+//POSTCONDICONES:
 void mostrarCartones(struct Cartones c[],int opcion );
+//PRE: NO TIENE.
+//POSTCONDICONES:
 int tipoDeCarton();
+//PRE: NO TIENE.
+//POSTCONDICONES:
 struct JugadorPc cargarJugadorPC(int cant);
+//PRE: NO TIENE.
+//POSTCONDICONES:
 int busqueda (int vector[], int cantidad, int buscar);
+//PRE: NO TIENE.
+//POSTCONDICONES:
 void cargarBolsa(int vector[], int cantidad);
+//PRE: NO TIENE.
+//POSTCONDICONES:
 void mostrarBolsa (int vector[], int cantidad);
+//PRE: NO TIENE.
+//POSTCONDICONES:
 void comienzoDelJuego(struct Jugador jugador1 , struct JugadorPc jugadorPc2 );
-void cantarColumna(struct Jugador jugador1 , int cant);
+//PRE: NO TIENE.
+//POSTCONDICONES:
+int cantarColumna(struct Jugador jugador1 , int cant);
+//PRE: NO TIENE.
+//POSTCONDICONES
+int cantarLinea(struct Jugador jugador1 , int cant);
 
 int main()
 {
@@ -57,7 +89,6 @@ int main()
     comienzoDelJuego(jugador1 , jugador2);
     return 0;
 }
-
 struct Cartones cargarCarton(int tipo){
     int i,x;
     int almacen=0;
@@ -113,8 +144,8 @@ struct Cartones cargarCarton(int tipo){
     return aux;
 
 }
-
-void mostrarCarton(struct Cartones e){
+void mostrarCarton(struct Cartones e)
+{
     int i,x;
     for (i=0; i<RENGLON; i++)
     {
@@ -134,7 +165,8 @@ void mostrarCarton(struct Cartones e){
 
 }
 
-int buscarNumeroRepe(int m[RENGLON][COLUMNA],int busco){
+int buscarNumeroRepe(int m[RENGLON][COLUMNA],int busco)
+{
     int posi = -1;
     int r,c;
     for (r=0; r<RENGLON; r++)
@@ -153,8 +185,8 @@ int buscarNumeroRepe(int m[RENGLON][COLUMNA],int busco){
 
 }
 
-void cargarCatones(struct Cartones c[],int opcion){
-
+void cargarCatones(struct Cartones c[],int opcion)
+{
 
     int i;
     int opciMA;
@@ -168,8 +200,8 @@ void cargarCatones(struct Cartones c[],int opcion){
     }
 
 }
-
-void mostrarCartones(struct Cartones c[],int opcion ){
+void mostrarCartones(struct Cartones c[],int opcion )
+{
     int i;
 
     for (i=0; i<opcion; i++)
@@ -182,7 +214,8 @@ void mostrarCartones(struct Cartones c[],int opcion ){
 
 }
 
-struct Jugador cargarJugador(){
+struct Jugador cargarJugador()
+{
     char auxNombre[20]= " ";
     char auxApellido[20]= " ";
     char auxDni[9] = " ";
@@ -231,7 +264,8 @@ void mostrarJugadores(struct Jugador p , struct JugadorPc maquina){
     mostrarCartones(maquina.cartonPc , maquina.numDeCartones);
 }
 
-int cuantosCartones(){
+int cuantosCartones()
+{
     int opci=0;
     system("cls");
     printf("\n------Seleccion de cartones------\n");
@@ -246,7 +280,8 @@ int cuantosCartones(){
     return opci;
 }
 
-int tipoDeCarton(){
+int tipoDeCarton()
+{
     int opci1;
     printf("\nQue tipo de cartones desea usar ?\n");
     printf(" 1 : Comprar cartones aleatorios\n");
@@ -263,6 +298,7 @@ int tipoDeCarton(){
     return opci1;
 
 }
+
 
 void cargarBolsa(int vector[], int cantidad){
     int numero = 0;
@@ -306,22 +342,28 @@ void mostrarBolsa (int vector[], int cantidad){
 
 }
 //PROCEDIMIENTO DA COMIENZO AL BUCLE DE JUEGO
-void comienzoDelJuego(struct Jugador jugador1 , struct JugadorPc jugadorPc2){
+void comienzoDelJuego(struct Jugador jugador1 , struct JugadorPc jugadorPc2 ){
     int colPuntos = 0;
+    int lineaPuntos = 0;
     jugador1.puntos = 0;
     int contadorBolillas = 0;
     int contadorBingoPc = 0;
     int contadorBingoJugador = 0;
+    int contadorColumnaJ = 0;
+    int contadorLineaJ = 0;
     int salida = -1;
     int cant = jugador1.numDeCartones;
+    printf("CANT : %d\n",cant);
     int i = 0;
     cargarBolsa(jugador1.bolillas , 90); //cargo la bolsa de bolillas
     for(int x = 0;x<5;x++){
         jugador1.columna1[x] = 0;
     }
+    for(int x = 0;x<3;x++){
+        jugador1.fila[x] = 0;
+    }
     do{
         printf("SALIO EL NUMERO ---> %d\n",jugador1.bolillas[i]);
-        printf("PUNTOSSSSSS : %d\n",jugador1.puntos);
         contadorBolillas = contadorBolillas + 1;
         sleep(1);
         for(int j = 0;j<cant;j++){
@@ -333,9 +375,8 @@ void comienzoDelJuego(struct Jugador jugador1 , struct JugadorPc jugadorPc2){
                             system("cls");
                             mostrarJugadores(jugador1,jugadorPc2);
                             printf("\nFELICIDADES SCASTE EL NUMERO %d\n",jugador1.bolillas[i]);
-                            cantarColumna(jugador1 , cant);
-                            jugador1.puntos +=1;
-                            //sleep(3);
+                            contadorBingoJugador = contadorBingoJugador + 1;
+                            sleep(3);
                         }
                         //CARTON JUGADOR MAQUINA
                         if(jugadorPc2.cartonPc[j].carton[r][c] == jugador1.bolillas[i]){
@@ -344,21 +385,39 @@ void comienzoDelJuego(struct Jugador jugador1 , struct JugadorPc jugadorPc2){
                             mostrarJugadores(jugador1,jugadorPc2);
                             printf("\nEL JUGADOR 2 SACO EL NUMERO %d\n",jugador1.bolillas[i]);
                             contadorBingoPc = contadorBingoPc + 1;
-                            //sleep(3);
+                            sleep(3);
                         }
                 }
             }
         }
+        contadorColumnaJ = cantarColumna(jugador1 , cant);
+        contadorLineaJ = cantarLinea(jugador1 , cant);
         i++;
         if(contadorBingoJugador == 15){
             printf("Felicidades GANASTE !\n");
-            jugador1.puntos = jugador1.puntos + 70;
-            printf("PUNTAJE : %d\n",jugador1.puntos);
+            colPuntos = colPuntos + 70;
+            for(int e = 0;e<contadorColumnaJ;e++){
+                colPuntos = colPuntos + 10;
+            }
+            for(int e = 0;e<contadorLineaJ;e++){
+                lineaPuntos = lineaPuntos + 20;
+            }
+            jugador1.puntos = jugador1.puntos + (((colPuntos + lineaPuntos)*contadorBolillas));
+            printf("Salieron %d bolillas\n",contadorBolillas);
+            printf("PUNTAJE : %f\n",jugador1.puntos);
             salida = 0;
         }
         if(contadorBingoPc == 15){
             printf("PERDISTE GANO LA PC! :( \n");
-            printf("PUNTAJE : %d\n",jugador1.puntos);
+            for(int e = 0;e<contadorColumnaJ;e++){
+                colPuntos = colPuntos + 10;
+            }
+            for(int e = 0;e<contadorLineaJ;e++){
+                lineaPuntos = lineaPuntos + 20;
+            }
+            jugador1.puntos = jugador1.puntos + (((colPuntos + lineaPuntos)*contadorBolillas));
+            printf("Salieron %d bolillas\n",contadorBolillas);
+            printf("PUNTAJE : %f\n",jugador1.puntos);
             salida = 0;
         }
     }while(salida == -1);
@@ -366,15 +425,33 @@ void comienzoDelJuego(struct Jugador jugador1 , struct JugadorPc jugadorPc2){
 
 //La logica de esta funcion anda bien lo unico que no puedo lograr es sumar los puntos
 //SI LAS POSICIONES DE LA MATRIZ SON -1 Y LA LA MATRIZ COLUMA ESTA EN 0(SIGNIFICA QUE NO FUE CANTADA) SUMA LOS PUNTOS
-void cantarColumna(struct Jugador jugador1 , int cant){
+int cantarColumna(struct Jugador jugador1 , int cant){
+    int contadorColumna = 0;
     for(int i = 0;i<cant;i++){
         for(int c=0;c<COLUMNA;c++){
             if((jugador1.vCarton[i].carton[0][c] == -1) && (jugador1.vCarton[i].carton[1][c] == -1) && (jugador1.vCarton[i].carton[2][c] == -1) && ((jugador1.columna1[c]) == 0) ){
-                jugador1.puntos += 10;
+                //jugador1.puntos = jugador1.puntos + 10;
                 jugador1.columna1[c] = -1;
+                contadorColumna = contadorColumna +  1;
                 printf("FELICIDADES HICISTE COLUMNA\n");
-                printf("putnos : %d\n",jugador1.puntos);
+
             }
         }
     }
+    return contadorColumna;
+}
+
+int cantarLinea(struct Jugador jugador1 , int cant){
+    int contadorLinea = 0;
+    for(int i = 0;i<cant;i++){
+        for(int r=0;r<RENGLON;r++){
+            if((jugador1.vCarton[i].carton[r][0] == -1) && (jugador1.vCarton[i].carton[r][1] == -1) && (jugador1.vCarton[i].carton[r][2] == -1) && (jugador1.vCarton[i].carton[r][3] == -1) && (jugador1.vCarton[i].carton[r][4] == -1) && ((jugador1.fila[r])) == 0){
+                jugador1.fila[r] = -1;
+                contadorLinea = contadorLinea + 1;
+                printf("FELICIDADES COMPLETASTE UNA FILA!!\n");
+
+            }
+        }
+    }
+    return contadorLinea;
 }
